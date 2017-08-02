a45b339df33becc9b00858549ae69df7aa747c66
Jessica <jdhpabst@gmail.com>
2017-08-02 13:23:51 -0600

    created component 'Create'

---
+++ src/components/Home/Create/Create.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import NewList from './NewList/NewList';
+
+import './Create.css';
+
+
+class Create extends Component {
+
+  render() {
+    return (
+      <div className="create">
+        
+      </div>
+    );
+  }
+}
+
+
+export default Create;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import NewList from './NewList/NewList';
+import Create from './Create/Create';
 
 import './Home.css';
 
@@ -10,7 +10,7 @@ class Home extends Component {
     return (
       <div className="home">
         <h1>Welcome!</h1>
-        {/* <NewList/> */}
+         <Create/> 
       </div>
     );
   }
