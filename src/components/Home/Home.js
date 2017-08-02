7ed52de6a254e808518a7d63ed3ded383e100892
Jessica <jdhpabst@gmail.com>
2017-08-02 13:09:44 -0600

    adding styling to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NewList from './NewList/NewList';
+// import NewList from './NewList/NewList';
 
 import './Home.css';
 
@@ -10,7 +10,7 @@ class Home extends Component {
     return (
       <div className="home">
         <h1>Welcome!</h1>
-        <NewList/>
+        {/* <NewList/> */}
       </div>
     );
   }
