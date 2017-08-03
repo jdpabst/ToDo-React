5b82d14ffdc0c228b1cea801daa7f550dc005861
Jessica <jdhpabst@gmail.com>
2017-08-03 15:37:59 -0600

    checkboxes are still not working properly

--- src/components/New/Custom/Custom.js
+++ src/components/New/Custom/Custom.js
@@ -14,6 +14,7 @@ class Custom extends Component {
             color: 'white',
             font: "Ubuntu', sans-serif",
             check: true,
+            checkbox: true,
             modal: false
         }
         this.handleColorYellow = this.handleColorYellow.bind(this);
@@ -124,6 +125,7 @@ class Custom extends Component {
                 modal: false,
             })
     }
+        ///////////////////////////// CHECK BOXES /////////////////////////
     handleCheck(){
         if(this.state.check){
             document.getElementById('check_img').style.display = 'block';
