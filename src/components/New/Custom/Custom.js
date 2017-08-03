d04e5aab1920febd60452336ec7d74a542760ed1
Jessica <jdhpabst@gmail.com>
2017-08-03 15:33:29 -0600

    working on checkboxes, only showing up on first item in list

--- src/components/New/Custom/Custom.js
+++ src/components/New/Custom/Custom.js
@@ -127,13 +127,17 @@ class Custom extends Component {
     handleCheck(){
         if(this.state.check){
             document.getElementById('check_img').style.display = 'block';
+            document.getElementById('checkbox').style.display = 'block';
             this.setState({
-                check: false
+                check: false,
+                checkbox: false
             })
         } else{
             document.getElementById('check_img').style.display = 'none';
+            document.getElementById('checkbox').style.display = 'none';
             this.setState({
-                check: true
+                check: true,
+                checkbox: true
             })
         }
     }
--- src/components/New/Custom/List/List.js
+++ src/components/New/Custom/List/List.js
@@ -29,11 +29,13 @@ class List extends Component {
     return (
       <div className="list">
         <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
+          <div id="checkbox"><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
           <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
         </form>
          {arr.map( (item, id) => {
           return <div key={ id }>
-            <form onSubmit={ (e) => this.handleList(e)}>
+            <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
+              <div id="checkbox"><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
               <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
             </form>
           </div>
