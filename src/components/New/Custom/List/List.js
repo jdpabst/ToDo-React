b9704adc5414c01e3b034d4e60712dd0bf1fb0ed
Jessica <jdhpabst@gmail.com>
2017-08-04 13:47:30 -0600

    working on more checkbox issues

--- src/components/New/Custom/List/List.js
+++ src/components/New/Custom/List/List.js
@@ -66,7 +66,8 @@ class List extends Component {
     return (
       <div className="list">
         <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
-          <div id="checkbox"><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
+           <div id="checkbox"><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div> 
+          {/* <input type="checkbox"/> */}
           <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
         </form>
         { map }
