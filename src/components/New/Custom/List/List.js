cbd0d2b9dd4ac745f4872b618d30f1fada1b545e
Jessica <jdhpabst@gmail.com>
2017-08-04 13:34:46 -0600

    got checkboxes appearing when/where they should, will work on checkoff

--- src/components/New/Custom/List/List.js
+++ src/components/New/Custom/List/List.js
@@ -22,24 +22,38 @@ class List extends Component {
       list: arr,
     })
   }
+
   render() {
     let arr = this.state.list;
     let font = this.props.font;
     let color = this.props.color;
+    let map;
+    if(this.props.checkFn){
+      map = arr.map( (item, id) => {
+          return <div key={ id }>
+            <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
+              <div id="checkbox" style={{display: 'none'}}><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
+              <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
+            </form>
+          </div>
+        })
+    } else {
+      map = arr.map( (item, id) => {
+          return <div key={ id }>
+            <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
+              <div id="checkbox" style={{display: 'block'}}><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
+              <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
+            </form>
+          </div>
+        }) 
+    }
     return (
       <div className="list">
         <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
           <div id="checkbox"><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
           <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
         </form>
-         {arr.map( (item, id) => {
-          return <div key={ id }>
-            <form onSubmit={ (e) => this.handleList(e)} autoSave="off">
-              <div id="checkbox"><img id="checkbox_img" src="https://vignette3.wikia.nocookie.net/roblox/images/5/57/Very-Basic-Checkmark-icon.png/revision/latest?cb=20131125154354"/></div>
-              <input id="list_input" style={{fontFamily: font, fontSize: '30px', color: color}}  type='text'/>
-            </form>
-          </div>
-        })} 
+        { map }
       </div>
     );
   }
